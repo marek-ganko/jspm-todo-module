2d27015d9cd9322b5771e03eec41db6ce7926c0c
Marek Gańko <ganko.marek@gmail.com>
2015-03-31 00:08:31 +0200

    jshint code conventions

--- src/AppDemo.js
+++ src/AppDemo.js
@@ -2,7 +2,7 @@ import 'angular';
 import TodoModules from './Todo';
 
 var app = angular.module('app', [TodoModules.name]);
-console.log("application : ", app.name, " started");
+console.log('application :' , app.name, ' started');
 angular.element(document).ready(function () {
   angular.bootstrap(document, [app.name]);
 });
