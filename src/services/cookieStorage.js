e16835e48433e11b2c7d7c82d7a5a48c076b2f39
Marek Gańko <ganko.marek@gmail.com>
2015-04-13 01:14:36 +0200

    git error

---
+++ src/services/cookieStorage.js
@@ -0,0 +1,31 @@
+'use strict';
+import {Storage} from './Storage';
+
+const STORAGE_ID = 'todo-module';
+
+export default class CookieStorage extends Storage {
+
+  constructor() {
+    super(this);
+  }
+
+  /**
+   * @returns {Array}
+   */
+  getFromStorage() {
+    let result = document.cookie.match(new RegExp(STORAGE_ID + '=([^;]+)'));
+    result && (result = JSON.parse(result[1]));
+    return result || [];
+    return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
+  }
+
+  /**
+   * @returns {Promise}
+   */
+  saveOnStorage(items) {
+    return new Promise((resolve, reject) => {
+      document.cookie = STORAGE_ID + '=' + JSON.stringify(items);
+      resolve(items);
+    });
+  }
+}
